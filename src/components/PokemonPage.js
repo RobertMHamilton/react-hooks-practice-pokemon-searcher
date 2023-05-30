e14dc6016c329857cd4049fec35f057d2d1e9a07
RobertMHamilton <127573177+RobertMHamilton@users.noreply.github.com>
2023-05-30 13:33:56 -0400

    Update PokemonPage.js

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -3,19 +3,31 @@ import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
+import { useState , useEffect } from "react";
 
 function PokemonPage() {
-  return (
-    <Container>
-      <h1>Pokemon Searcher</h1>
-      <br />
-      <PokemonForm />
-      <br />
-      <Search />
-      <br />
-      <PokemonCollection />
-    </Container>
-  );
-}
+
+ const[pokemons , setPokemons]= useState([])
+ const[searchQuery , setSearchQuery]=useState("")
+ const handleOnChange = (e)=> setSearchQuery(e.target.value)
+ 
+ useState(()=>{
+   fetch("http://localhost:3001/pokemon")
+   .then(resp => resp.json())
+   .then((pokemons) => setPokemons(pokemons))
+ },[])
+
+    return (
+      <Container>
+        <h1>Pokemon Searcher</h1>
+        <br />
+        <PokemonForm setPokemons={setPokemons}/>
+        <br />
+        <Search handleOnChange={handleOnChange} />
+        <br />
+        <PokemonCollection pokemons={pokemons} searchQuery={searchQuery} />
+      </Container>
+    );
+  }
 
 export default PokemonPage;
