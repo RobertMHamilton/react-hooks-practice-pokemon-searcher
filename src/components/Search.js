2756c7ec3be32b567371f241c4c0414f44215c50
RobertMHamilton <127573177+RobertMHamilton@users.noreply.github.com>
2023-05-30 13:35:10 -0400

    Update Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,13 @@
 import React from "react";
 
-function Search() {
+
+function Search({handleOnChange}) {
+  
+
   return (
     <div className="ui search">
       <div className="ui icon input">
-        <input className="prompt" />
+        <input className="prompt" onChange={handleOnChange}/>
         <i className="search icon" />
       </div>
     </div>
