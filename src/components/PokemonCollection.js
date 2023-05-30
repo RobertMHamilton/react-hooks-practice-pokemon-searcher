1718856ff3c8042075f24431c50b92796c2ed3ea
RobertMHamilton <127573177+RobertMHamilton@users.noreply.github.com>
2023-05-30 13:34:42 -0400

    Update PokemonCollection.js

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -2,10 +2,15 @@ import React from "react";
 import PokemonCard from "./PokemonCard";
 import { Card } from "semantic-ui-react";
 
-function PokemonCollection() {
+function PokemonCollection({pokemons , searchQuery}) {
+  
+  const searchResults = pokemons.filter((pokemon)=>{
+    return pokemon.name.toLowerCase().includes(searchQuery.toLowerCase())
+  })
+
   return (
     <Card.Group itemsPerRow={6}>
-      <h1>Hello From Pokemon Collection</h1>
+    {searchResults.map(pokemon=> <PokemonCard key ={pokemon.id}{...pokemon}/>)}
     </Card.Group>
   );
 }
